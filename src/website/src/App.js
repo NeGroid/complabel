d9a8296e178f76e10c508066e67bb5f19e15e109
Nikolai Pristupa <micolaprs@gmail.com>
2019-06-14 16:42:20 +0300

    Remove logo.

--- src/website/src/App.js
+++ src/website/src/App.js
@@ -1,14 +1,11 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import SolutionForm from "./SolutionForm";
 
 function App() {
   return (
     <div className="container-fluid">
-      <header className="navbar navbar-expand navbar-dark flex-column bd-navbar">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
+      <header className="navbar navbar-expand navbar-dark flex-column bd-navbar"></header>
       <div className="col-12 col-md-3 col-xl-2 bd-sidebar"></div>
       <main
         className="col-12 col-md-9 col-xl-8 py-md-3 pl-md-5 bd-content"
