fb4d704f0b552804a35029464ce6a1b4f485d40c
Nikolai Pristupa <micolaprs@gmail.com>
2019-06-19 14:02:08 +0300

    Move the solution form path to the root route.

--- src/website/src/App.js
+++ src/website/src/App.js
@@ -1,22 +1,21 @@
 import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import "./App.css";
 import SolutionForm from "./SolutionForm";
 
 function App() {
   return (
     <div className="container-fluid">
-      <header className="navbar navbar-expand navbar-dark flex-column bd-navbar"></header>
-      <div className="col-12 col-md-3 col-xl-2 bd-sidebar"></div>
-      <main
-        className="col-12 col-md-9 col-xl-8 py-md-3 pl-md-5 bd-content"
-        role="main"
-      >
-        <h1>Solution Finder</h1>
-        <p>Fill the form to find a solution</p>
-        <div className="bd-example">
-          <SolutionForm />
-        </div>
-      </main>
+      <Router>
+        <header className="navbar navbar-expand navbar-dark flex-column bd-navbar"></header>
+        <div className="col-12 col-md-3 col-xl-2 bd-sidebar"></div>
+        <main
+          className="col-12 col-md-9 col-xl-8 py-md-3 pl-md-5 bd-content"
+          role="main"
+        >
+          <Route path="/" component={SolutionForm} />
+        </main>
+      </Router>
     </div>
   );
 }
